140d2b96f7ade0654a41ddb1edc4adbdac1d1a49
toledompm <toledompm@gmail.com>
2021-01-07 16:45:19 -0300

    publish route

--- api-message-producer/src/app.controller.ts
+++ api-message-producer/src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post(':name')
+  async publishName(@Param() name: string): Promise<void> {
+    return await this.appService.publishMessage(name);
   }
 }
